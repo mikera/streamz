57a4c944b89741590fb8733a3929336bfc6e498e
mikera <mike.r.anderson.13@gmail.com>
2013-07-14 21:48:28 +0100

    add cljunit test

---
+++ src/test/java/mikera/streamz/AllClojureTests.java
@@ -0,0 +1,12 @@
+package mikera.streamz;
+
+import mikera.cljunit.ClojureTest;
+
+public class AllClojureTests extends ClojureTest {
+	
+	@Override
+	public String filter() {
+		return "mikera.streamz";
+	}
+
+}
