8040e2a4c4ce5fa3ab1aaf47a0120ef680a59e4d
mikera <mike.r.anderson.13@gmail.com>
2013-07-14 21:55:36 +0100

    map of servers

--- src/main/clojure/mikera/streamz/server.clj
+++ src/main/clojure/mikera/streamz/server.clj
@@ -7,6 +7,13 @@
      :headers {"Content-Type" "text/html"}
      :body    "Hello World!"}))
 
+;; store the current server in a map of port -> close function
+(def SERVER_MAP (agent {}))
+
 (defn launch []
-  (let [close-fn (hts/run-server (fresh-handler) {:port 8080})]
-    close-fn))
+  (send-off SERVER_MAP 
+    (fn [m]
+      (let [port 8080
+            _ (when (m port) ((m port))) ;; close the existing server, if any
+            close-fn (hts/run-server (fresh-handler) {:port port})]
+        (assoc m port close-fn)))))
