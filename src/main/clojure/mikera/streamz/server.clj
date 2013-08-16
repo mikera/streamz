6b88092255a6169b89b3ff2e3e0ce25a9d7e94ab
mikera <mike.r.anderson.13@gmail.com>
2013-08-16 09:24:49 +0800

    improved server handling with stop and stop-all

--- src/main/clojure/mikera/streamz/server.clj
+++ src/main/clojure/mikera/streamz/server.clj
@@ -1,4 +1,5 @@
 (ns mikera.streamz.server
+  (:use [mikera.cljutils.error])
   (:require [org.httpkit.server :as hts]))
 
 (defn fresh-handler []
@@ -8,15 +9,32 @@
      :body    "Hello World!"}))
 
 ;; store the current server in a map of port -> close function
-(def SERVER_MAP (agent {}))
+(def SERVER-MAP (agent {}))
 
-(defn launch 
+(defn start 
   "Launches a streamz server on the given port with default configuration"
-  ([] (launch 8080))
+  ([] (start 8080))
   ([port]
-    (send-off SERVER_MAP 
+    (send-off SERVER-MAP 
               (fn [m]
       (let [port (int port)
             _ (when (m port) ((m port))) ;; close the existing server, if any
             close-fn (hts/run-server (fresh-handler) {:port port})]
         (assoc m port close-fn))))))
+
+
+(defn stop-all
+  "Stops the streamz server on all ports"
+  ([] (send-off SERVER-MAP
+                (fn [m]
+                  (doseq [[port cf] m] (cf))
+                  {}))))
+
+(defn stop
+  "Stops the streamz server on all ports or a specific port"
+  ([] (stop-all))
+  ([port] (send-off SERVER-MAP
+                (fn [m]
+                  (if-let [cf (m port)]
+                    (do (cf) (dissoc m port))
+                    (error "Streamz port " port " not running"))))))
